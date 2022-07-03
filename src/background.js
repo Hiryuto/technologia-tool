5d599c2d522b9c4c3bd1e8a86468c0b0ad42475e
2288-256 <hiramatsu.yuto.highschool@gmail.com>
2022-07-03 23:44:48 +0900

    commit

--- src/background.js
+++ src/background.js
@@ -1,6 +1,10 @@
 chrome.runtime.onInstalled.addListener((details) => {
 	if (details.reason === "install") {
 		chrome.tabs.create({ url: "installed.html" });
+		//初期化
+		chrome.storage.local.set({ CharacterImg: "" });
+		chrome.storage.local.set({ TempImg: "" });
+		chrome.storage.local.set({ authorization: "" });
 	}
 	if (details.reason === "update") {
 		chrome.tabs.create({ url: "update.html" });
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,15 @@
-window.onload = chrome.tabs.create({ url: "options.html" });
+async function main() {
+	var request = await fetch(
+		"https://register.technologia-schoolofmagic.jp/api/players",
+		{
+			method: "GET",
+			//   modes: "cors",
+			headers: {
+				authorization: "9781cb39-2abf-43d2-b15a-668386ef9742",
+			},
+		}
+	);
+	var data = await request.json();
+	document.getElementById("level").textContent = `${data.owner_id}`;
+}
+main();
