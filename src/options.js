a26b2d304f70900e5a831b1edba962eb946787c0
Hiryuto <hiramatsu.yuto.highschool@gmail.com>
2022-07-04 15:12:13 +0900

    #5 #12

--- src/options.js
+++ src/options.js
@@ -2,88 +2,106 @@ var CharacterImgs;
 var CharacterImg;
 var TempImg;
 var Message = document.getElementById("message");
-function save_options() {
-	if (document.querySelector("#message")) {
-		alert("画像を指定してください");
-	} else {
-		chrome.storage.local.get(["TempImg"], function (result) {
-			TempImg = result.TempImg;
-		});
-		setTimeout(() => {
-			chrome.storage.local.set({ CharacterImg: TempImg }, function () {
-				var status = document.getElementById("status");
-				status.innerHTML = `<h5 style="margin-top: 20px;">設定を保存しました。</h5>`;
-				setTimeout(function () {
-					status.textContent = "";
-				}, 5000);
-			});
-		}, 1000);
-	}
+function save_img() {
+  if (document.querySelector("#message")) {
+    alert("画像を指定してください");
+  } else {
+    chrome.storage.local.get(["TempImg"], function (result) {
+      TempImg = result.TempImg;
+    });
+    setTimeout(() => {
+      chrome.storage.local.set({ CharacterImg: TempImg }, function () {
+        var status = document.getElementById("status-img");
+        status.innerHTML = `<h5 style="margin-top: 20px;">設定を保存しました。</h5>`;
+        setTimeout(function () {
+          status.textContent = "";
+        }, 5000);
+      });
+    }, 1000);
+  }
+}
+function save_authorization() {
+  //   alert(document.getElementById("authorization").value);
+  if (!document.getElementById("authorization").value.match(/\S/g)) {
+    alert("認証コードを指定してください。");
+  } else {
+    var authorization = document.getElementById("authorization").value;
+    chrome.storage.local.set({ authorization: authorization }, function () {
+      var status = document.getElementById("status-authorization");
+      status.innerHTML = `<h5 style="margin-top: 20px;">設定を保存しました。</h5>`;
+      setTimeout(function () {
+        status.textContent = "";
+      }, 5000);
+    });
+  }
 }
 function Textdecision() {
-	let text = document.getElementById("url").value;
-	var urlcheck1 =
-		/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g;
-	if (!text.match(urlcheck1)) {
-		return (document.querySelector(
-			"#uploadImageArea"
-		).innerHTML = `<h5 id="message" class="message" style="color:red;">URLを入力してください</h5>`);
-	} else {
-		document.getElementById("uploadImage").value = "";
-		document.getElementById("filename").textContent = "選択されていません";
-		document.querySelector("#uploadImageArea").innerHTML =
-			"<h6>しばらくお待ち下さい。</h6>";
-		ImageTempsave(text);
-	}
+  let text = document.getElementById("url").value;
+  var urlcheck1 =
+    /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g;
+  if (!text.match(urlcheck1)) {
+    return (document.querySelector(
+      "#uploadImageArea"
+    ).innerHTML = `<h5 id="message" class="message" style="color:red;">URLを入力してください</h5>`);
+  } else {
+    document.getElementById("uploadImage").value = "";
+    document.getElementById("filename").textContent = "選択されていません";
+    document.querySelector("#uploadImageArea").innerHTML =
+      "<h6>しばらくお待ち下さい。</h6>";
+    ImageTempsave(text);
+  }
 }
 
 function changeImageToBase64() {
-	document.getElementById("url").value = "";
-	document.querySelector("#uploadImageArea").innerHTML =
-		"<h6>しばらくお待ち下さい。</h6>";
-	const uploadImage = document.querySelector("#uploadImage");
-	const file = uploadImage.files[0];
-	const reader = new FileReader();
-	reader.onload = (event) => {
-		const base64Text = event.currentTarget.result;
-		var value = base64Text;
-		ImageTempsave(value);
-	};
-	reader.readAsDataURL(file);
+  document.getElementById("url").value = "";
+  document.querySelector("#uploadImageArea").innerHTML =
+    "<h6>しばらくお待ち下さい。</h6>";
+  const uploadImage = document.querySelector("#uploadImage");
+  const file = uploadImage.files[0];
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    const base64Text = event.currentTarget.result;
+    var value = base64Text;
+    ImageTempsave(value);
+  };
+  reader.readAsDataURL(file);
 }
 function ImageTempsave(saveValue) {
-	chrome.storage.local.set({ TempImg: saveValue });
-	setTimeout(function () {
-		chrome.storage.local.get(["TempImg"], function (result) {
-			CharacterImgs = result.TempImg;
-			document.querySelector(
-				"#uploadImageArea"
-			).innerHTML = `<img src="${CharacterImgs}" id="image" width="222" height="auto" max-width="100%"
+  chrome.storage.local.set({ TempImg: saveValue });
+  setTimeout(function () {
+    chrome.storage.local.get(["TempImg"], function (result) {
+      CharacterImgs = result.TempImg;
+      document.querySelector(
+        "#uploadImageArea"
+      ).innerHTML = `<img src="${CharacterImgs}" id="image" width="222" height="auto" max-width="100%"
 			height="auto"vertical-align= "middle"width= "180px"
 			aspect-ratio= "auto 180 / 300"/>`;
-			document.getElementById("image").addEventListener("error", function () {
-				document.querySelector(
-					"#uploadImageArea"
-				).innerHTML = `<h5 id="message" class="message" style="color:red;margin-bottom: 20px;">画像が正常に読み込めませんでした。<br><br>&lt;原因&gt;<br><br>URL指定の場合:<a href="${saveValue}" target="_blank" style="color:red;">リンク先</a>に画像がない<br>画像選択の場合:選択した画像にアクセスできない</h5>`;
-			});
-		});
-	}, 1000);
+      document.getElementById("image").addEventListener("error", function () {
+        document.querySelector(
+          "#uploadImageArea"
+        ).innerHTML = `<h5 id="message" class="message" style="color:red;margin-bottom: 20px;">画像が正常に読み込めませんでした。<br><br>&lt;原因&gt;<br><br>URL指定の場合:<a href="${saveValue}" target="_blank" style="color:red;">リンク先</a>に画像がない<br>画像選択の場合:選択した画像にアクセスできない</h5>`;
+      });
+    });
+  }, 1000);
 }
 document
-	.getElementById("uploadImage")
-	.addEventListener("change", function (evt) {
-		var file = evt.target.files;
-		const maxFileSize = 9997544;
-		if (file[0].size > maxFileSize) {
-			document.querySelector(
-				"#uploadImageArea"
-			).innerHTML = `<h5 id="message" class="message" style="color:red;">ファイルサイズは10MB以下にしてください</h5>`;
-			fileInput.value = "";
-			return;
-		}
-		changeImageToBase64();
-		document.getElementById("filename").textContent = file[0].name;
-	});
+  .getElementById("uploadImage")
+  .addEventListener("change", function (evt) {
+    var file = evt.target.files;
+    const maxFileSize = 9997544;
+    if (file[0].size > maxFileSize) {
+      document.querySelector(
+        "#uploadImageArea"
+      ).innerHTML = `<h5 id="message" class="message" style="color:red;">ファイルサイズは10MB以下にしてください</h5>`;
+      fileInput.value = "";
+      return;
+    }
+    changeImageToBase64();
+    document.getElementById("filename").textContent = file[0].name;
+  });
 
-document.getElementById("save").addEventListener("click", save_options);
+document.getElementById("save-img").addEventListener("click", save_img);
+document
+  .getElementById("save-authorization")
+  .addEventListener("click", save_authorization);
 document.getElementById("decision").addEventListener("click", Textdecision);
--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,39 @@
-async function main() {
-	var request = await fetch(
-		"https://register.technologia-schoolofmagic.jp/api/players",
-		{
-			method: "GET",
-			//   modes: "cors",
-			headers: {
-				authorization: "9781cb39-2abf-43d2-b15a-668386ef9742",
-			},
-		}
-	);
-	var data = await request.json();
-	document.getElementById("level").textContent = `${data.owner_id}`;
+chrome.storage.local.get(["authorization"], async function (result) {
+  authorization = result.authorization;
+  if (authorization == undefined) {
+    return (document.getElementById("status").innerHTML =
+      "<h1>認証コードが設定されていません。</h1>");
+  }
+  textContent("load", "読み込み中");
+  var request = await fetch(
+    "https://register.technologia-schoolofmagic.jp/api/players",
+    {
+      method: "GET",
+      //   modes: "cors",
+      headers: {
+        authorization: `${authorization}`,
+      },
+    }
+  );
+  var data = await request.json();
+  console.log(request.status);
+  if (request.status !== 200) {
+    return innerHTML(
+      "status",
+      `<h1>正常に取得できませんでした。</h1><h1>ステータスコード:${request.status}</h1>`
+    );
+  }
+  var last_time_iso = new Date(data.lesson_progress.play_days.last_time);
+  var last_time = last_time_iso.toLocaleString();
+  textContent("load", "");
+  textContent("level", data.statuses.designation.rank);
+  textContent("label", data.statuses.designation.label);
+  document.getElementById("last_time").textContent = `${last_time}`;
+});
+
+function textContent(id, message) {
+  document.getElementById(id).textContent = `${message}`;
 }
-main();
+function innerHTML(id, message) {
+  document.getElementById(id).innerHTML = `${message}`;
+}
